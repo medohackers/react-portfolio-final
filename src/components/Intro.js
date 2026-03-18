2d105bf1d3509d7febba8a713ceabc9fc7cf505c
medohackers <135126620+medohackers@users.noreply.github.com>
2026-03-18 23:47:51 +0200

    Refactor Intro.js for improved styling and structure

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,73 +1,73 @@
 import React from 'react'
 import styled from 'styled-components'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 import Me from '../assets/Images/profile-img.png'
 
-
 const Box = styled(motion.div)`
-
-position: absolute;
-left: 50%;
-top: 50%;
-transform: translate(-50%, -50%);
-
-
-width: 65vw;
-height:55vh;
-display: flex;
-
-
-background: linear-gradient(
-    to right,
-    ${props => props.theme.body} 50%,
-    ${props => props.theme.text} 50%) bottom,
-    linear-gradient(
-    to right,
-    ${props => props.theme.body} 50%,
-    ${props => props.theme.text} 50%) top;
-    background-repeat: no-repeat;
-background-size: 100% 2px;
-    border-top: 2px solid ${props => props.theme.body};
-    border-botton: 2px solid ${props => props.theme.text};
-
-
-    z-index:1;
-
-`
-const SubBox = styled.div`
-width: 50%;
-position: relative;
-display: flex;
-
-.pic{
     position: absolute;
-    bottom: 0;
     left: 50%;
-    transform: translate(-50%,0%);
-    width: 100%;
-    height: auto;
-}
+    top: 50%;
+    transform: translate(-50%, -50%);
+
+    width: 65vw;
+    height: 55vh;
+    display: flex;
+
+    background: linear-gradient(
+        to right,
+        ${props => props.theme.body} 50%,
+        ${props => props.theme.text} 50%) bottom,
+        linear-gradient(
+        to right,
+        ${props => props.theme.body} 50%,
+        ${props => props.theme.text} 50%) top;
+    background-repeat: no-repeat;
+    background-size: 100% 2px;
+    border-left: 2px solid ${props => props.theme.body};
+    border-right: 2px solid ${props => props.theme.text};
+
+    z-index: 1;
 `
 
-const Text = styled.div`
-font-size: calc(1em + 1.5vw);
-color: ${props => props.theme.body};
-padding: 2rem;
-cursor: pointer;
+const SubBox = styled.div`
+    width: 50%;
+    position: relative;
+    display: flex;
+    align-items: center; /* لضبط العناصر في المنتصف عمودياً */
+`
 
-display: flex;
-flex-direction: column;
-justify-content: space-evenly;
+const TextLeft = styled.div`
+    font-size: calc(1em + 1.5vw);
+    color: ${props => props.theme.body};
+    padding: 2rem;
+    display: flex;
+    flex-direction: column;
+    z-index: 3;
+`
 
-&>*:last-child{
-    color: ${props => `rgba(${props.theme.bodyRgba},0.6)` };
+const TextRight = styled.div`
     font-size: calc(0.5rem + 1.5vw);
-    font-weight:300;
+    color: ${props => props.theme.text}; /* تم استخدام عكس اللون ليكون مرئياً في الجانب الأيمن */
+    padding: 2rem;
+    display: flex;
+    flex-direction: column;
+    font-weight: 300;
+    z-index: 3;
+`
 
-}
+const ImageContainer = styled(motion.div)`
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: 40%; /* يمكنك زيادة أو تقليل هذه النسبة لتكبير/تصغير الصورة كما تحب */
+    height: auto;
+    z-index: 2; /* وضعنا الصورة في المنتصف تماماً فوق الخلفية وتحت النصوص */
 
-
-
+    .pic {
+        width: 100%;
+        height: auto;
+    }
 `
 
 const Intro = () => {
@@ -78,20 +78,24 @@ const Intro = () => {
         transition={{ type: 'spring', duration:2, delay:1 }}
         >
             <SubBox>
-                <Text>
+                <TextLeft>
                     <h1>Hi,</h1>
                     <h3>I'm Carlos Michel.</h3>
-                    <h6>I am a programmer and a trader in the crypto market.</h6>
-                </Text>
+                </TextLeft>
             </SubBox>
-            <SubBox>
-                <motion.div
+
+            <ImageContainer
                 initial={{opacity:0}}
-        animate={{opacity: 1}}
-        transition={{ duration:1, delay:2 }}
-                >
-                    <img className="pic" src={Me} alt="Profile Pic" />
-                </motion.div>
+                animate={{opacity: 1}}
+                transition={{ duration:1, delay:2 }}
+            >
+                <img className="pic" src={Me} alt="Profile Pic" />
+            </ImageContainer>
+
+            <SubBox>
+                <TextRight>
+                    <h6>I am a programmer and a trader in the crypto market.</h6>
+                </TextRight>
             </SubBox>
         </Box>
     )
