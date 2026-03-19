f9cdc031c6cee2878cf4b21c55ed65c31cc9da28
medohackers <135126620+medohackers@users.noreply.github.com>
2026-03-19 15:56:42 +0200

    Update BlogData.js

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -35,7 +35,7 @@ export const Blogs = [
     },
     {
         id:5,
-        name:"What is Higher Order Component (HOC) in React JS?",
+        name:"مراقبه السهم الذهب وتوقع \n عالي",
         tags:["react","reactJS","web-development"],
         date:"3 Feb, 2021",
         imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625295480148%2FlyKj8bpHK.jpeg%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
