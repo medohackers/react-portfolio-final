5a68fc832b0170c3ca7900ef4bbf6383fd16feaf
medohackers <135126620+medohackers@users.noreply.github.com>
2026-03-19 16:47:38 +0200

    Update WorkData.js

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -3,61 +3,24 @@
 export const Work = [
     {
         id:1,
-        name:"Agency Landing Page",
-        description:"It is build on top of the React JS, with styledComponents and GSAP for smooth scrolling animations.",
-        tags:["react","gsap","styledComponents"],
+        name:"TradingView",
+        description:"ترادينج فيو هي منصة تحليل فني عبر الإنترنت توفر أدوات متقدمة لتحليل الأسواق المالية، زي الأسهم، العملات، العملات الرقمية، والسلع. بتتيح لك إنشاء مخططات بيانية ومتابعة الأسواق بشكل مباشر، مع ميزات متعددة تلبي احتياجات المتداولين ¹.",
+        tags:["USDT","GOLD","Trading"],
         
-        demo:"https://agency-website-eta.vercel.app/",
-        github:"https://github.com/codebucks27/Agency-website"
+        demo:"https://apps.apple.com/eg/app/tradingview-%D8%AA%D8%A7%D8%A8%D8%B9-%D8%AC%D9%85%D9%8A%D8%B9-%D8%A7%D9%84%D8%A3%D8%B3%D9%88%D8%A7%D9%82/id1205990992?l=ar",
+       
     },
     {
         id:2,
-        name:"Job listing website",
-        description:"A Job listing website build in ReactJS. With searching and filtering functionalities.",
-        tags:["react","sass"],
+        name:"MT5",
+        description:"MT5 هي منصة تداول إلكترونية متقدمة طورتها شركة ميتا كوتس، بتتيح للمتداولين الوصول إلى الأسواق المالية العالمية، وتحليل الأسعار، وتنفيذ الصفقات بسهولة. تعتبر ترقية للمنصة السابقة MT4، وتقدم ميزات إضافية زي أدوات تحليل فني متقدمة، وتداول في أسواق متعددة. 😊.",
+        tags:["USDT","GOLD","Trading"],
         
-        demo:"http://react-job-listing-website.codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/react-job-listing-website"
-        
-    },
-    {
-        id:3,
-        name:"Dev.to Clone",
-        description:"A clone of Dev.to build using ReactJS. With realtime fetching of articles from dev.to using API.",
-        tags:["react","api","sass"],
-        
-        demo:"http://devto-clone-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/devto-clone"
-    },
-    {
-        id:4,
-        name:"Sidebar Menu",
-        description:"A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
-        tags:["react","styledComponents","react-router"],
-        
-        demo:"https://react-sidebar.vercel.app/",
-        github:"https://github.com/codebucks27/react-sidebar"
+        demo:"https://apps.apple.com/eg/app/metatrader-5/id413251709?l=ar",
        
-    },{
-        id:5,
-        name:"Todo App(React + Redux)",
-        description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
-        tags:["react","redux","framer-motion"],
-        
-        demo:"http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/React-Redux-Todo-App"
         
     },
-    {
-        id:6,
-        name:"Responsive Navigation Menu",
-        description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
-        tags:["react","react-router","css"],
-        
-        demo:"http://react-responsive-navbar-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/React-responsive-navbar"
-        
-    },
+    
     
   
 ] 
