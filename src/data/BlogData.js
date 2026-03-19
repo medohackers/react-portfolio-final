fbe16e006d54c96b1474d76d7cec9166fa0131e5
medohackers <135126620+medohackers@users.noreply.github.com>
2026-03-19 14:57:07 +0200

    Update BlogData.js

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -11,19 +11,19 @@ export const Blogs = [
     },
     {
         id:2,
-        name:"How to create React Hamburger Menu using Styled-Components",
-        tags:["react","styled-components"],
-        date:"5 July, 2021",
-        imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625479747640%2F7KzwP9nmj.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
-        link:"https://codebucks.hashnode.dev/react-hamburger-menu"
+        name:"•السعر عمل تصحيح (ABC) ونزل لمنطقة طلب قوية (OB + FVG).•	حصل تجميع بعدها وانطلاق قوي لفوق (اتجاه صاعد).•اي رجوع للدعم ممكن يكون فرصة شراء طالما مفيش كسر.",
+        tags:["XAUUSD","GOLD","Trading"],
+        date:"14 Oct, 2025",
+        imgSrc:"https://files.catbox.moe/83q95r.jpg",
+        link:""
     },
     {
         id:3,
-        name:"React Loading Screen: Try these 3 cool loading screens for your app",
-        tags:["react","react-lottie","styled-components"],
-        date:"6 July, 2021",
-        imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625552344293%2Fegwis0UIX.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
-        link:"https://codebucks.hashnode.dev/react-loading-screens"
+        name:"مراقبه السهم وتوقع رفع لسهم مره اخري",
+        tags:["XAUUSD","GOLD","Trading"],
+        date:"7 Dec, 2025",
+        imgSrc:"https://files.catbox.moe/skic2d.jpg",
+        link:""
     },
     {
         id:4,
