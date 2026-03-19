fde9b6ac47aba463a2a680986210f17ffafaf336
medohackers <135126620+medohackers@users.noreply.github.com>
2026-03-19 14:26:34 +0200

    Fix import statement for framer-motion

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-HI { motion } from 'framer-motion'
+import { motion } from 'framer-motion'
 import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import styled, { keyframes } from 'styled-components'
