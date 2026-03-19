96e57a7c75451b45a4da7ba3db61e04203fe8f3c
medohackers <135126620+medohackers@users.noreply.github.com>
2026-03-19 14:13:34 +0200

    Update greeting text to Arabic

--- src/components/Main.js
+++ src/components/Main.js
@@ -159,7 +159,7 @@ const Main = () => {
                 whileTap={{scale: 0.9}}
                 
                 >
-                    Say hi..
+                    قل مرحبا ✋🏻
                 </motion.h2>
             </Contact>
             <BLOG to="/blog">
