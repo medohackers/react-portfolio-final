93e932419df195af447cdd2b1ae2acf6531cc88c
medohackers <135126620+medohackers@users.noreply.github.com>
2026-05-01 21:32:49 +0300

    Update Intro.js

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -24,4 +24,125 @@ const Box = styled(motion.div)`
     background-repeat: no-repeat;
     background-size: 100% 2px;
     border-left: 2px solid ${props => props.theme.body};
-    border-right: 2px solid ${props =>
+    border-right: 2px solid ${props => props.theme.text};
+
+    z-index: 1;
+
+    @media (max-width: 768px) {
+        width: 85vw;
+        height: 65vh;
+    }
+`
+
+const SubBox = styled.div`
+    width: 50%;
+    position: relative;
+    display: flex;
+    flex-direction: column; 
+
+    /* فصلنا التنسيق هنا: اللاب توب في النص، والموبايل يرجع فوق */
+    @media (min-width: 769px) {
+        justify-content: center; 
+    }
+    @media (max-width: 768px) {
+        justify-content: flex-start;
+    }
+`
+
+const TextLeft = styled.div`
+    font-size: calc(1em + 1.5vw);
+    color: ${props => props.theme.body};
+    padding: 2rem;
+    display: flex;
+    flex-direction: column;
+    z-index: 3;
+
+    @media (min-width: 769px) {
+        max-width: 75%; 
+    }
+
+    @media (max-width: 768px) {
+        padding: 1rem;
+        padding-top: 2rem; /* نزلناه سيكا عشان ميبقاش لازق في الخط اللي فوق */
+        font-size: calc(1em + 2vw);
+    }
+`
+
+const TextRight = styled.div`
+    font-size: calc(1rem + 1.5vw);
+    color: ${props => props.theme.text};
+    font-weight: 300;
+    z-index: 3;
+
+    @media (min-width: 769px) {
+        position: absolute;
+        top: 2rem;
+        right: 1.5rem;
+        text-align: right;
+        max-width: 60%; 
+    }
+
+    @media (max-width: 768px) {
+        position: relative;
+        padding: 1rem;
+        padding-top: 2rem; /* عشان يبقى موازي للنص اللي على الشمال في الموبايل */
+        font-size: calc(1rem + 2vw);
+        text-align: left;
+        width: 100%;
+    }
+`
+
+const ImageContainer = styled(motion.div)`
+    position: absolute;
+    bottom: 0; 
+    left: 50%;
+    transform: translate(-50%, 0);
+    height: 85%; 
+    display: flex;
+    align-items: flex-end; 
+    z-index: 2;
+
+    .pic {
+        max-height: 100%; 
+        width: auto; 
+        max-width: 100%;
+        object-fit: contain;
+    }
+
+    @media (max-width: 768px) {
+        height: 90%; 
+    }
+`
+
+const Intro = () => {
+    return (
+        <Box
+        initial={{height:0}}
+        animate={{height: '50vh'}}
+        transition={{ type: 'spring', duration:2, delay:1 }}
+        >
+            <SubBox>
+                <TextLeft>
+                    <h1>Hi,</h1>
+                    <h3>I'm Carlos Misheil.</h3>
+                </TextLeft>
+            </SubBox>
+            
+            <ImageContainer
+                initial={{opacity:0}}
+                animate={{opacity: 1}}
+                transition={{ duration:1, delay:2 }}
+            >
+                <img className="pic" src={Me} alt="Profile Pic" />
+            </ImageContainer>
+            
+            <SubBox>
+                <TextRight>
+                    <h6>𝑰 𝒂𝒎 𝒂 𝒑𝒓𝒐𝒈𝒓𝒂𝒎𝒎𝒆𝒓 𝒂𝒏𝒅 𝒂 𝒕𝒓𝒂𝒅𝒆𝒓 𝒊𝒏 𝒕𝒉𝒆 𝒄𝒓𝒚𝒑𝒕𝒐 𝒎𝒂𝒓𝒌𝒆𝒕.</h6>
+                </TextRight>
+            </SubBox>
+        </Box>
+    )
+}
+
+export default Intro
